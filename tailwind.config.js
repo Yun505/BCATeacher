76da83d5bd69b069188079f06c4f5ca119ac4ea9
Yun <yunjun505@gmail.com>
2022-06-13 14:47:38 -0400

    hope

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./index.html"],
     theme: {
+      colors:{
+        transparent: 'transparent',
+        sky: colors.sky,
+        black: colors.black,
+        orange: colors.orange,
+        white: colors.white,
+        green: colors.green,
+      },
       extend: {},
     },
     plugins: [],
